3be6c22a76c35db9aa226a4e5621ea3c5d50620b
jugesh raghav <74092377+jugeshraghav@users.noreply.github.com>
2024-06-17 07:21:01 +0530

    debouncing in js

--- JS_Interview/debouncing.js
+++ JS_Interview/debouncing.js
@@ -1,5 +1,24 @@
-console.log("hello from Debouncing");
+// document.addEventListener("DOMContentLoaded", function () {
+// const input = document.getElementById("searchInput");
+// const getData = () => {
+//   console.log("fetching data");
+// };
+// input.addEventListener("input", getData);
+// })
 
-function getData() {
-  console.log("Fetching Data.");
-}
+const getData = () => {
+  console.log("fetching data");
+};
+
+const debounce = (fn, d) => {
+  let timer;
+  return function (...args) {
+    if (timer) clearTimeout(timer);
+    let n = args[0];
+    timer = setTimeout(() => {
+      fn(n);
+    }, d);
+  };
+};
+
+const efficientGetData = debounce(getData, 800);
